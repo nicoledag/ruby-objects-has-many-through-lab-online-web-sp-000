42d59ad3dd7b75686f10ee74adbb972776cdf67e
Nicole D <nnardella@gmail.com>
2018-11-28 17:52:13 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Artist
+
+attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select {|song| song.artist}
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def genres
+
+  end
+
+end
